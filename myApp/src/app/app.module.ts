2b82fb08661cceb65a7588a8daebb6466c80a2f4
Brendan Whiting <brendan.whiting@me.com>
2016-12-10 11:34:50 -0500

    first commit

---
+++ myApp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { CaretakerPage } from '../pages/caretaker/caretaker';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    CaretakerPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    CaretakerPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
